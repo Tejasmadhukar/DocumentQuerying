523bd19a829ab7795246c70a611c47a2d50a1cc0
Tejas013 <tejasmadhukar6@gmail.com>
2023-06-23 18:04:52 +0530

    Updated Landing Page

--- app/page.tsx
+++ app/page.tsx
@@ -12,13 +12,13 @@ export default function Home() {
 		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
 			<div className="inline-block max-w-lg text-center justify-center">
 				<h1 className={title()}>Make&nbsp;</h1>
-				<h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
+				<h1 className={title({ color: "violet" })}>Documents&nbsp;</h1>
 				<br />
 				<h1 className={title()}>
-					websites regardless of your design experience.
+					More fun with Chat GPT.
 				</h1>
 				<h2 className={subtitle({ class: "mt-4" })}>
-					Beautiful, fast and modern React UI library.
+					Fast, modern and reliable Document querying tool.
 				</h2>
 			</div>
 
@@ -27,27 +27,11 @@ export default function Home() {
 					isExternal
 					as={NextLink}
 					href={siteConfig.links.docs}
-					className={buttonStyles({ color: "primary", radius: "full", variant: "shadow" })}
+					className={buttonStyles({ color: "primary", radius: "full", variant: "ghost" })}
 				>
-					Documentation
+					Continue
 				</Link>
-				<Link
-					isExternal
-					as={NextLink}
-					className={buttonStyles({ variant: "bordered", radius: "full" })}
-					href={siteConfig.links.github}
-				>
-					<GithubIcon size={20} />
-					GitHub
-				</Link>
-			</div>
-
-			<div className="mt-8">
-				<Snippet hideSymbol hideCopyButton variant="bordered">
-					<span>
-						Get started by editing <Code color="primary">app/page.tsx</Code>
-					</span>
-				</Snippet>
+	
 			</div>
 		</section>
 	);

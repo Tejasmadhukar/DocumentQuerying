6d9dfaaadbfa4ddb54d5976cae2f5ff80afcebae
Tejas013 <tejasmadhukar6@gmail.com>
2023-06-25 02:14:07 +0530

    Updated Dispatch

--- redux/store.ts
+++ redux/store.ts
@@ -7,4 +7,4 @@ export const store = configureStore({
 })
 
 export type RootState = ReturnType<typeof store.getState>;
-export type AppState = ReturnType<typeof store.dispatch>;
+export type AppDispatch = ReturnType<typeof store.dispatch>;

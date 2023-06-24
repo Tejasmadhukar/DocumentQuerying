1934d05e589751fb7bfae40a9cc8bcbcc10df01b
Tejas013 <tejasmadhukar6@gmail.com>
2023-06-24 23:41:27 +0530

    Added Server Actions

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = nextConfig = {
+    experimental: {
+        serverActions: true,
+    },
+}

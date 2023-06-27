3721b871296d3d38ac87d931893e715f2af13063
Tejas013 <tejasmadhukar6@gmail.com>
2023-06-27 21:52:51 +0530

    Added spinner

---
+++ app/chat/loading.tsx
@@ -0,0 +1,10 @@
+"use client"
+import {Spinner} from "@nextui-org/react";
+
+export default function App() {
+  return (
+    <div className="xl" style={{height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <Spinner />
+    </div>
+  );
+}

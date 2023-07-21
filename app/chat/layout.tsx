6bd1b25be832751dcd60147c0fe6ad4622c48a31
Tejas013 <tejasmadhukar6@gmail.com>
2023-07-21 07:16:59 +0530

    Fix this

--- app/chat/layout.tsx
+++ app/chat/layout.tsx
@@ -2,6 +2,29 @@ import { authConfig } from "@/config/auth";
 import { getServerSession } from "next-auth";
 import NotAllowedChat from "@/components/Chat/NotAllowedChat";
 import Sidebar from "@/components/Chat/Sidebar";
+import { GoodDivider,GoodSpinner } from "@/components/NextuiClient";
+import { Suspense } from "react";
+import { prisma } from "@/config/db";
+import Link from "next/link";
+
+async function Titles ({ UserID }: { UserID: string }){
+
+    const titles = await await prisma.messageGroup.findMany({
+        where:{
+            userId: UserID,
+        }
+    })
+
+    return (
+        <>
+            <div className="flex flex-col mt-2">
+                {titles.map((title, index) => 
+                    <Link key={index} className="text-purple-500 mt-4" href={`/chat/${title.id}`}>{title.Title}</Link>
+                )}
+            </div>
+        </>
+    )
+}
 
 export default async function ChatLayout(props: {
 	children: React.ReactNode,
@@ -21,7 +44,13 @@ export default async function ChatLayout(props: {
 	return (
         <>
 			<div className="pt-16 min-h-screen flex">
-				<Sidebar session={session}/>
+				<div className="w-1/6 flex flex-col items-center">
+					<Sidebar session={session}/>
+					<Suspense fallback={<p>Loading...</p>}>
+						<Titles UserID={session.user.id}/>
+					</Suspense>
+				</div>
+				<GoodDivider />
 				{props.children}
 			</div>
         </>
--- components/Chat/Sidebar.tsx
+++ components/Chat/Sidebar.tsx
@@ -1,56 +1,23 @@
-import { prisma } from "@/config/db";
 import { Button } from "@nextui-org/button";
 import { Session } from "next-auth";
 import Link from "next/link";
-import { Spinner } from "@nextui-org/react";
-import { FC, Suspense } from "react";
+import { FC } from "react";
 // import { PlusIcon } from "../icons"; Reduce size of this icon so it looks nice 
 
 interface SidebarProps {
     session: Session,
 }
 
-async function Titles ({ email }: { email: string }){
 
-    const user = await prisma.user.findUnique({
-        where:{
-            email: email
-        }
-    })
-
-    const titles = await prisma.messageGroup.findMany({
-        where:{
-            userID: user?.userID
-        }
-    })
-
-    console.log(titles)
-
-    return (
-        <>
-            {titles.map((title) => (
-                <>
-                    <h1>{title.Title}</h1>
-                </>
-            ))}
-        </>
-    )
-}
 
 const Sidebar:FC<SidebarProps> = ({session}) => {
-    const email = session.user?.email;
-
-    if(!email) return <p>Get out</p>
-
+    const id = session.user.id;
 
     return (
         <>
-            <div className="w-1/6 flex flex-col items-center">
-                <Button className=" mt-4" as={Link} href="/chat" color="secondary" variant="ghost">New Chat</Button>
-                {/* <Suspense fallback={<div>Loading...</div>}>
-                    <Titles email={email}/>
-                </Suspense> */}
-            </div>
+            
+            <Button className=" mt-4" as={Link} href="/chat" color="secondary" variant="ghost">New Chat</Button>
+              
             
         </>
     )

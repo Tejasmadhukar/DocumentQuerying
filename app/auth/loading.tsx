77b2771334d13a3e3dc25feb40fd8b0648da720a
Tejas013 <tejasmadhukar6@gmail.com>
2023-07-24 15:10:35 +0530

    Updated repo

--- app/auth/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function App() {
-  return (
-    <div className="xl" style={{height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-        <h1>Add server side loader here</h1>
-    </div>
-  );
-}
--- app/chat/[id]/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Chat ({params}: {params: {id: number}}) {
-    return (
-        <>
-            <h1>You came here by hard soft</h1>
-        </>
-    )
-}

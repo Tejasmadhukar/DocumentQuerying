ffcb0b63ac5892ac8b31fb9de9b1438541a399a2
Tejas013 <tejasmadhukar6@gmail.com>
2023-08-08 23:25:04 +0530

    added react query prefetching

--- NextJsApp/components/Chat/Sidebar/Sidebar.tsx
+++ NextJsApp/components/Chat/Sidebar/Sidebar.tsx
@@ -1,43 +1,44 @@
+import getQueryClient from "@/app/getQueryClient";
 import { prisma } from "@/config/db";
 import { Button } from "@nextui-org/button";
+import { Hydrate, dehydrate } from "@tanstack/react-query";
 import { Session } from "next-auth";
 import Link from "next/link";
-import { FC, Suspense } from "react";
-import { GoodSpinner } from "../../NextuiClient";
+import { FC } from "react";
+import SidebarItems from "./SidebarItems";
 // import { PlusIcon } from "../icons"; Reduce size of this icon so it looks nice 
 
 interface SidebarProps {
     session: Session,
 }
 
-async function Titles ({ UserID }: { UserID: string }){
+async function getTitles (UserID : string){
     const titles = await prisma.messageGroup.findMany({
         where:{
             userId: UserID,
         }
     })
 
-    return (
-        <>
-            <div className="flex flex-col mt-2">
-                {titles.map((title, index) => 
-                    <Link key={index} className="text-purple-500 mt-4" href={`/chat/${title.id}`}>{title.Title}</Link>
-                )}
-            </div>
-        </>
-    )
+    return titles
 }
 
 
-const Sidebar:FC<SidebarProps> = ({session}) => {
+const Sidebar:FC<SidebarProps> = async ({session}) => {
+
+    const userId = session.user.id;
+
+    const queryClient = getQueryClient()
+    await queryClient.prefetchQuery(['titles'], () => getTitles(userId))
+
+    const dehydratedState = dehydrate(queryClient)
 
     return (
         <>
             <div className="w-1/6 flex flex-col items-center">
                 <Button className="mt-4" as={Link} href="/chat" color="secondary" variant="ghost">New Chat</Button>
-                <Suspense fallback={<div className="mt-20"><GoodSpinner /></div>}>
-                    <Titles UserID={session.user.id}/>
-                </Suspense>
+                <Hydrate state={dehydratedState}>
+                    <SidebarItems userId={userId}/>
+                </Hydrate>
             </div>
         </>
     )

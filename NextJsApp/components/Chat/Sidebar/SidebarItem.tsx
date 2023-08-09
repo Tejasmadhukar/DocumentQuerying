922c6cd7ec3642ff34bd2a741484481d3c677b3e
Tejas013 <tejasmadhukar6@gmail.com>
2023-08-09 21:51:24 +0530

    added optimistic update to delete.

--- NextJsApp/components/Chat/Sidebar/SidebarItem.tsx
+++ NextJsApp/components/Chat/Sidebar/SidebarItem.tsx
@@ -1,15 +1,72 @@
+import { BinIcon } from "@/components/icons"
+import { Spacer } from "@nextui-org/react"
 import { MessageGroup } from "@prisma/client"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import Link from "next/link"
+import { FC } from "react"
 
 interface SidebarItemProps{
     title : MessageGroup
 }
 
-export default function SidebarItem (props: SidebarItemProps) {
+async function deleteTitle (id: string) {
+    try {
+        const res = await fetch(`/api/titles/${id}`,{
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+    
+        if(!res.ok) {
+            throw new Error(`deletion failed: ${id}`)
+        }
+    } catch (error) {
+        console.log(error)
+        throw new Error('something went wrong, contact tejasmadhukar6@gmail.com')
+    }
+}
+
+const SidebarItem:FC<SidebarItemProps> = ({title}) => {
+    const queryClient = useQueryClient()
+
+    const deleteMutation = useMutation({
+        mutationFn: deleteTitle,
+        onMutate: async (id) => {
+            await queryClient.cancelQueries({ queryKey: ['title'] });
+            const previousTitles = queryClient.getQueryData<MessageGroup[]>(['title'])
+
+            let updatedTitles: MessageGroup[] = []
+            if(previousTitles){
+                updatedTitles = [...previousTitles].filter((deletion_title) => deletion_title.id !== id)
+            }
+
+            queryClient.setQueryData<MessageGroup[]>(['title'], updatedTitles)
+
+            return { previousTitles };
+        },
+
+        onError: (context: {previousTitles?: MessageGroup[] | undefined}) => {
+            queryClient.setQueryData<MessageGroup[]>(['title'], context.previousTitles)
+        },
+
+        onSettled: () => {
+            queryClient.invalidateQueries(['title'])
+        }
+    })
+    
     return (
         <>
-            <div className=" mt-6 ">
-                <p>hi</p>
+            <div className=" group flex mt-6 cursor-pointer">
+                <Link href={`/chat/${title.id}`}>{title.Title}</Link>
+                <Spacer x={2}/>
+                <div className="opacity-0 group-hover:opacity-100 transition-opacity" onClick={() => deleteMutation.mutate(title.id)} >
+                    <BinIcon />
+                </div>
+                
             </div>
         </>
     )
 }
+
+export default SidebarItem;

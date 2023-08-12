69e854cf50af2ba38f82304260830be457310d28
Tejas013 <tejasmadhukar6@gmail.com>
2023-08-12 16:23:06 +0530

    client chat works perfectly

--- NextJsApp/components/Chat/Chat.tsx
+++ NextJsApp/components/Chat/Chat.tsx
@@ -1,40 +1,62 @@
 'use client'
-import { MessageGroup } from "@/types";
-import { useState } from "react";
+import { Spinner } from "@nextui-org/react";
+import { Message } from "@prisma/client";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useEffect, useRef } from "react";
 import ChatInput from "./ChatInput";
-import Upload from "./FileUploading/Upload";
 import TextMessage from "./Text";
 
-export default function ChatPage({groupId, InitialMessages} : {groupId: string, InitialMessages: MessageGroup[]}) {
-    const [messages, setMessages] = useState<MessageGroup[]>([]);
-    
-    const handleSendMessage = (message: MessageGroup) => {
-      setMessages((prevMessages) => [...prevMessages, message]);
-    };
+async function getMessages(group: string) {
+    const res = await fetch(`/api/chat/${group}`);
+    if (!res.ok) {
+        console.log(res)
+    } else {
+        return await res.json()
+    }
+}
 
-    const SetLoading = (key: boolean) => {
-        if(key){
-            handleSendMessage({message: 1, user: 'bot'})
-        }else{
-            setMessages((prevMessages) => {
-                const updatedMessages = [...prevMessages];
-                updatedMessages.pop(); 
-                return updatedMessages;
-              });
+export default function ChatPage({groupId} : {groupId: string}) {
+    const {data, isLoading} = useQuery<Message[]>(['messages'], () => getMessages(groupId))
+    const queryClient = useQueryClient()
+    const bottomRef = useRef<HTMLDivElement>(null)
+
+    useEffect(()=>{
+        bottomRef.current?.scrollIntoView({behavior: 'smooth'});
+    },[])
+
+    useEffect(()=>{
+        bottomRef.current?.scrollIntoView({behavior: 'smooth'});
+    },[data])
+
+    const ScrollintoView = () => {
+        bottomRef.current?.scrollIntoView({behavior: 'smooth'});
+    }
+
+    if(isLoading) return <div className="flex justify-center items-center"><Spinner /></div>
+
+    if(!data) return <h1>No corresponding text messages exsist !!</h1>
+
+    if(data.length === 0){
+        const previousMessages = queryClient.getQueryData<Message[]>(['messages'])
+        const welcomeMessage:Message = {id: 'first', created_by: 'bot', content_message: 'You can query your files now !!', groupId, updated_at: new Date()}
+
+        let updatedMessages:Message[] = []
+        if(previousMessages){
+            updatedMessages = [...previousMessages, welcomeMessage]
         }
+        queryClient.setQueryData<Message[]>(['messages'], updatedMessages)
     }
     
     return (
-        <>
-            <div className="flex flex-col h-screen message group">
-                {messages.length <= 0 ? (
-                    <Upload />
-                ) : (
-                    <TextMessage Messages={messages}/>
+        <div className="flex w-full flex-col h-screen message group">
+            <div className="flex-1 overflow-y-auto">
+                {data.map((message,index)=>
+                    <TextMessage key={index} message={message}/>
                 )}
-
-                <ChatInput onSendMessage={handleSendMessage} MessageLength={messages.length} Loading={SetLoading}/>
+                <div ref={bottomRef} />
             </div>
-        </>
+            
+            <ChatInput scrollFunction={ScrollintoView} groupId={groupId}/>
+        </div>
     )
 }

ecb1cfe7a4aab0dbf4888beb51c4cdaf82b29dd2
Tejas013 <tejasmadhukar6@gmail.com>
2023-08-12 16:22:39 +0530

    minor change

--- NextJsApp/components/NextuiClient.tsx
+++ NextJsApp/components/NextuiClient.tsx
@@ -1,12 +1,11 @@
 'use client'
 
-import { Spinner } from "@nextui-org/react"
-import { Divider } from "@nextui-org/react"
+import { Divider, Spinner } from "@nextui-org/react"
 
 export function GoodSpinner () {
     return <Spinner />
 }
 
 export function GoodDivider () {
-    return <Divider orientation="vertical" className=" h-screen mr-5 border-1"/>
+    return <Divider orientation="vertical" className=" h-screen border-1"/>
 }

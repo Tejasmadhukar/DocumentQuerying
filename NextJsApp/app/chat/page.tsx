f8c0c53590d2fa23a613c249cef17c87a00c9f45
Tejas013 <tejasmadhukar6@gmail.com>
2023-08-12 16:22:16 +0530

    minor change

--- NextJsApp/app/chat/page.tsx
+++ NextJsApp/app/chat/page.tsx
@@ -1,16 +1,13 @@
 import Upload from "@/components/Chat/FileUploading/Upload"
 import { title } from "@/components/primitives"
 export default function Chat() {
-
     return (
-        <>
-            <section className="flex-1 flex flex-col items-center justify-center mb-40 ">
-                <div className="max-w-lg  justify-center gap-4 ">
-                    <h1 className={title({ color: "violet" })}>Upload&nbsp;</h1>
-                    <h1 className={title()}>your files here&nbsp;</h1>
-                </div>
-                <Upload />
-            </section>
-        </>
+        <section className="flex-1 flex flex-col items-center justify-center mb-40 ">
+            <div className="max-w-lg  justify-center gap-4 ">
+                <h1 className={title({ color: "violet" })}>Upload&nbsp;</h1>
+                <h1 className={title()}>your files here&nbsp;</h1>
+            </div>
+            <Upload />
+        </section>
     )
 }

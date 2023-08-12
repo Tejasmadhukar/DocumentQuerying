015a9bb61949f24b46bc375d4e82f4c19732ce4a
Tejas013 <tejasmadhukar6@gmail.com>
2023-08-12 16:22:04 +0530

    added spinner

--- NextJsApp/app/chat/[id]/loading.tsx
+++ NextJsApp/app/chat/[id]/loading.tsx
@@ -1,7 +1,10 @@
+import { GoodSpinner } from "@/components/NextuiClient";
 export default function ChatpageLoading() {
   return (
-    <div className="xl" style={{height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-        <h1>Add server side loader here</h1>
-    </div>
+    <section className="flex-1 flex flex-col items-center justify-center mb-40 ">
+      <div className="max-w-lg  justify-center gap-4 ">
+        <GoodSpinner />
+      </div>
+    </section>
   );
 }
